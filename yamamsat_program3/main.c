7462de1ec71eb86feceef64e2ed699b207d53d7f
OSUsatoru <yamamsat@oregonstate.edu>
2021-02-08 06:56:43 -0800

    save progress

--- yamamsat_program3/main.c
+++ yamamsat_program3/main.c
@@ -44,6 +44,7 @@
 #include <limits.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -60,16 +61,38 @@ const int max_charactor = 2049;
 */
 const int max_arg = 513;
 
+/* reset for new input */
 struct arst{
     int num_arg;
     _Bool backgroundCommand;
     char *InputFile;
     char *OutputFile;
 };
+/* unchanged status*/
+struct gloval_arst{
+    _Bool active_SIGTSTP;
+    int exit_status;
+};
 
+/* list of bg_process */
+/*
+struct bg_process{
+    pid_t bg_processes;
+    struct bg_process *next;
+};*/
+/* this is not efficient */
+/*int bg_process[128];*/
+int num_bg_process = 0;
+/*
+struct bg_process *head = NULL;
+struct bg_process *tail = NULL;
+*/
+
+void check_bgprocess(struct gloval_arst *glar);
 
 void input_argument(char *input_args[], struct arst *arg_status);
-int process_arguments(char *input_argus[], struct arst *arg_status);
+int process_arguments(char *input_argument[], struct arst *arg_status, struct gloval_arst *glar);
+void execute_command(char *input_argument[], struct arst *arg_status, struct gloval_arst *glar);
 
 /*
     There variable are chenged by each input
@@ -82,7 +105,10 @@ int main(void) {
     /*store all arguments in line*/
     char *input_args[max_arg];
     int isContinue = 1;
+    /* initialize all elements */
+    /*memset(bg_process, -1, sizeof(bg_process));*/
 
+    struct gloval_arst gloval_status = {0, 1};
 
     do{
         //printf("check: %d", arg_status->check);
@@ -94,9 +120,17 @@ int main(void) {
 
         /*check if the argument is comment or empty*/
         if(input_args[0][0] != 35 && arg_status.num_arg!= 0){
-            isContinue = process_arguments(input_args, &arg_status);
+            isContinue = process_arguments(input_args, &arg_status, &gloval_status);
             if(isContinue == 2){
                 /*use execvp*/
+                /*
+                    my pc
+                    execvp(input_args[0], const char * const*(input_args));
+                    or  on os server
+                    execvp (const char *__file, char *const __argv[])
+                */
+                execute_command(input_args, &arg_status, &gloval_status);
+                //execvp(input_args[0], (const char * const*)input_args);
             }
         }
 
@@ -115,12 +149,59 @@ int main(void) {
         for(int i = 0; i < arg_status.num_arg; ++i){
             input_args[i] = NULL;
         }
+        check_bgprocess(&gloval_status);
+
     }while(isContinue);
 
 
 
 
     return 0;
+}
+/*
+    check the back ground process
+    if the process is terminated, display message and delete it from list
+****************************************************************************/
+void check_bgprocess(struct gloval_arst *glar)
+{
+        int spawnPid;
+        //int cnt = num_bg_process;
+		while ( (spawnPid = waitpid(-1, &(glar->exit_status), WNOHANG)) > 0){
+			printf("background pid %d terminated is done: ", spawnPid);
+                fflush(stdout);
+
+            /*normal*/
+            if (WIFEXITED(glar->exit_status)){
+                printf("exit value %d\n", WEXITSTATUS(glar->exit_status));
+                fflush(stdout);
+			} /*abnormal*/
+            else if(WIFSIGNALED(glar->exit_status)){
+                printf("terminated by signal %d", WTERMSIG(glar->exit_status));
+                fflush(stdout);
+            }
+        }
+
+        /*
+        for(int i = 0; i < 128; ++i){
+            if(bg_process[i] == -1){
+                break;
+            }else if(waitpid(bg_process[i], &(glar->exit_status) , WNOHANG) > 0){
+                printf("background pid %d terminated is done: ", bg_process[i]);
+                fflush(stdout);
+            }
+
+            if (WIFEXITED(glar->exit_status)){
+                printf("exit value %d\n", bg_process[i], WEXITSTATUS(glar->exit_status));
+                fflush(stdout);
+			}
+            else if(WIFSIGNALED(glar->exit_status)){
+                printf("terminated by signal %d", WTERMSIG(glar->exit_status));
+                fflush(stdout);
+            }
+            bg_process[i] = -1;
+        }
+        */
+
 }
 /*
     This receive input line by line and store each argument
@@ -160,7 +241,7 @@ void input_argument(char *input_args[],  struct arst *arg_status)
 
 
     /* getpid()*/
-    int num_pid = 1234;
+    int num_pid = getpid();
     char pid[32];
     /* for replacment, convert PID to string*/
     sprintf(pid, "%d", num_pid);
@@ -239,7 +320,7 @@ void input_argument(char *input_args[],  struct arst *arg_status)
     3. status
     4. other
 ******************************************************************/
-int process_arguments(char *input_argus[], struct arst *arg_status)
+int process_arguments(char *input_argus[], struct arst *arg_status, struct gloval_arst *glar)
 {
     /*
     printf("%s\n", input_argus[0]);
@@ -301,9 +382,99 @@ int process_arguments(char *input_argus[], struct arst *arg_status)
 
         return 1;
     }else if(!strcmp(input_argus[0],"status")){
+        if(WIFEXITED(glar->exit_status)){
+            printf("exit value %d\n", WEXITSTATUS(glar->exit_status));
+            fflush(stdout);
+		} /*abnormal*/
+        else if(WIFSIGNALED(glar->exit_status)){
+            printf("terminated by signal %d", WTERMSIG(glar->exit_status));
+            fflush(stdout);
+        }
         return 1;
     }
     else{
         return 2;
     }
+}
+/*
+    This executes other commands
+    if there is input or output file, try open and redirect stdout and stdin
+    if backgroundCommand = 1 and  active_SIGTSTP = 0, do not need to wait for child
+    else need to wait for child
+**************************************************************************/
+void execute_command(char *input_argument[], struct arst *arg_status, struct gloval_arst *glar)
+{
+    int in_fdescriptor = 0, out_fdescriptor = 0;
+    int result;
+    pid_t spawnPid = -5;
+
+    spawnPid = fork();
+	switch (spawnPid) {
+		case -1:
+			perror("fork() failed!");
+			exit(1);
+			break;
+		case 0:
+            /* if need to redirect input output */
+			if(arg_status->InputFile != NULL){
+                in_fdescriptor = open(arg_status->InputFile, O_RDONLY);
+                /* if failed open file*/
+				if (in_fdescriptor == -1){
+					printf("cannot open %s for input\n", arg_status->InputFile);
+                    fflush(stdout);
+					exit(1);
+				}
+                result = dup2(in_fdescriptor, 0);
+                /* if dup2 failed*/
+				if (result == -1){
+					perror("dup2()\n");
+					exit(2);
+				}
+            }
+            if(arg_status->OutputFile != NULL){
+                out_fdescriptor = open(arg_status->OutputFile, O_CREAT|O_WRONLY|O_APPEND, 0666);
+                /* if failed open file*/
+				if (out_fdescriptor == -1){
+					printf("cannot open %s for output\n", arg_status->OutputFile);
+                    fflush(stdout);
+					exit(1);
+				}
+                result = dup2(out_fdescriptor, 1);
+                /* if dup2 failed*/
+				if (result == -1){
+					perror("dup2()\n");
+					exit(2);
+				}
+            }
+
+
+
+			execvp(input_argument[0], (char* const*)input_argument);
+
+            /*error if child reachs here*/
+			printf("%s: no such file or directory\n", input_argument[0]);
+            fflush(stdout);
+            /*close file*/
+            close(in_fdescriptor);
+            close(out_fdescriptor);
+			exit(1);
+
+
+            break;
+
+		default:
+
+            /* if there was & and SIGTSTP is not active*/
+			if (arg_status->backgroundCommand && glar->active_SIGTSTP == 0) {
+                /* no need to wait for this child */
+				pid_t childPid = waitpid(spawnPid, &(glar->exit_status) , WNOHANG);
+				printf("background pid is %d\n", spawnPid);
+				fflush(stdout);
+			}
+			// Otherwise execute it like normal
+			else {
+				pid_t childPid = waitpid(spawnPid, &(glar->exit_status), 0);
+			}
+
+	}
 }
